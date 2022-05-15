26261552b2e9b6ba1b539412afa0a76785db2ebf
plowsof <77655812+plowsof@users.noreply.github.com>
2022-05-15 02:34:10 +0100

    Update post_logs.py

--- post_logs.py
+++ post_logs.py
@@ -7,26 +7,29 @@ import pprint
 #https://libera.monerologs.net/monero-dev/20220511
 
 git_token = "hunter2hunter2"
-issue_number = 704
+issue_number = 703
 git_repo = "monero-project/meta"
-moderator_name = "plowsof[m]"
-room = "monero-community"
-
-msg_begin = "meeting start"
-msg_end = "meeting over"
-
+#moderator_name = "plowsof[m]"
+moderator_name = "ErCiccione"
+#room = "monero-community"
+room = "monero-dev"
+#msg_begin = "meeting start"
+#msg_end = "meeting over"
+msg_begin = "Hey folks"
+msg_end = "the meeting is over, but obviously feel free to continue the conversations above 🙂"
 def post_comment(comment):
     global issue_number, git_repo, git_token
     g = Github(git_token)
     repo = g.get_repo(git_repo)
     issue = repo.get_issue(number=issue_number)
     print(issue.state) 
+    pprint.pprint(comment)
     issue.create_comment(f"Logs \n```\n{comment}\n```\nAutomated by [this](https://github.com/plowsof/post-libera-meeting-logs)")
     issue.edit(state="closed")
-    
+
 def get_meeting_log():
     global room, moderator_name, msg_begin, msg_end
-    todays_date = datetime.datetime.now().strftime("%Y%m%d")
+    todays_date = datetime.datetime.now().strftime("%Y%m14")
     meeting_status = "none"
     log = ""
     r = requests.get(f"https://libera.monerologs.net/{room}/{todays_date}/raw")
@@ -37,10 +40,10 @@ def get_meeting_log():
             username = split[1]
             comment = line.split(f"{split[0]} {split[1]}")[1][1:]
             if username == f"<{moderator_name}>":
-                if comment == msg_begin:
+                if msg_begin in comment:
                     print("begin")
                     meeting_status = "active"
-                if comment == msg_end:
+                if msg_end in comment:
                     meeting_status = "ended"
             if meeting_status == "active":
                 log += f"{line}\n"
@@ -51,4 +54,6 @@ def get_meeting_log():
                 break
                 
 get_meeting_log()
+
+
 
